77031ba33190e1dd3dbe16275f46e75378d634ab
Shaquon <shaquonkelley@gmail.com>
2019-06-26 13:26:59 -0400

    Quon~ made some more file structure fixes. Connected app to redux. Created Home page. Made reducer and actions folders. Basic skeleton is set up and ready to go.

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Home extends Component {
+  
+    render() { 
+        return (  
+            <div>
+                Bid Buddies
+            </div>
+        );
+    }
+}
+ 
+export default Home;
