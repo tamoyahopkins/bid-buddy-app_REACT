76c2ef64a89cfa089c96c822350b4052ad790c1c
Tamoya Hopkins <tamoyashopkins@gmail.com>
2019-07-09 21:43:19 -0400

    commented out all code in bid-buddy/src folder

--- bid-buddy/src 2/REDUCERS/index.js
+++ bid-buddy/src 2/REDUCERS/index.js
@@ -1,8 +1,8 @@
-import { combineReducers } from "redux";
-import { connectRouter } from "connected-react-router";
+// import { combineReducers } from "redux";
+// import { connectRouter } from "connected-react-router";
 
 
-export default history =>
-  combineReducers({
-    router: connectRouter(history)
-  });
+// export default history =>
+//   combineReducers({
+//     router: connectRouter(history)
+//   });
