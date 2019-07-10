83cb0d7396b7e73aad39d501cff5ae3c8ac12f8e
Tamoya Hopkins <tamoyashopkins@gmail.com>
2019-07-09 22:38:32 -0400

    fixing issue

---
+++ src/components/Veiws/PricingPage.js
@@ -0,0 +1,111 @@
+
+import React, {Component} from 'react'; 
+import './PricingPage.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Grid, Image, Form } from 'semantic-ui-react'
+import logo from "../../images/bid_buddy_logo_3.png";
+import handShake from "../../images/agreementMetIcon.png"
+
+const ReactDOM = require('react-dom'); 
+const modalRoot = document.getElementById('modal-root');
+
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.el = document.createElement('div');
+  }
+  componentDidMount() {modalRoot.appendChild(this.el);}
+  componentWillUnmount() {modalRoot.removeChild(this.el);}
+  render() {
+     return ReactDOM.createPortal(
+      this.props.children,
+      this.el,
+    );
+  }
+}
+
+class PricingPage extends Component {
+    state = {showModal: false};
+  
+
+  handleShow = ()=> {
+    this.setState({showModal: true });
+  }
+  handleClose = ()=> {
+    let newShowModal = !this.state.showModal
+    this.setState({showModal: newShowModal }); 
+  }
+  
+  // handleHide = () => {this.setState({showModal: false});
+  // }
+
+    render(){
+      const modal = this.state.showModal ? (
+        <Modal>
+          <div className="modal">
+            <img class="modal_clientLogo" src="https://s3-media4.fl.yelpcdn.com/bphoto/KImy2lcnme23Q8jeUQ7s_A/ls.jpg" ></img>
+            <div class="modal_userForm">
+              <form onSubmit={this.handleClose} id="modal_Form">
+                <label>Listed Price: </label>
+                <input></input>
+                <label>Enter Bid Amount:</label>
+                <input></input>
+                
+                <h3>Payment Details</h3>
+                <label>Payee Name:</label>
+                <input></input>
+                <label>Card Number:</label>
+                <input></input>
+                <label>Expiration Date:</label>
+                <input></input>
+                <label>CBD:</label>
+                <input></input>
+                <hr></hr>
+                <button type="submit">Confirm Bid!</button>
+              </form>
+            </div>
+          </div>
+        </Modal>
+      ) : null;
+      
+        return(
+          <div id="pricingPage-Container">
+            <img id='pricingPageLogo'src={logo}/> 
+            <h2 id="pricingPage-Header">LOWEST PRICE: </h2>
+            <h2 id="lowestPriceDiv">$0</h2>    
+            <h2 id='pricingPage-Header-2'>AVAILABLE AT BELOW STORES: </h2>       
+            {/* pricefeed div */}
+            <div id="priceFeed-Container">
+              <div id="storeInfoDiv-left"><span>1.</span></div>
+              <div id="storeInfoDiv-middle"><span id="storeName">TJ Maxx </span></div>
+              <div id="storeInfoDiv-middle2"><span id="storePrice">$0</span></div>
+              <button onClick={this.handleShow} id="storeInfoDiv-right"><img id="bidButtonImage" src={handShake}/> {modal}</button>
+            </div>
+            <div id="priceFeed-Container">
+              <div id="storeInfoDiv-left"><span>2.</span></div>
+              <div id="storeInfoDiv-middle"><span id="storeName">Walmart </span></div>
+              <div id="storeInfoDiv-middle2"><span id="storePrice">$00,000.00</span></div>
+              <button id="storeInfoDiv-right"><img id="bidButtonImage" src={handShake}/></button>
+            </div>
+            <div id="priceFeed-Container">
+              <div id="storeInfoDiv-left"><span>3.</span></div>
+              <div id="storeInfoDiv-middle"><span id="storeName">Walmart </span></div>
+              <div id="storeInfoDiv-middle2"><span id="storePrice">$00,000.00</span></div>
+              <button id="storeInfoDiv-right"><img id="bidButtonImage" src={handShake}/></button>
+            </div>
+            {/* BidButton */}
+            
+
+            <button id="scanNewItemButton"><bold>Scan Another Item</bold></button>    
+        </div>
+
+
+
+        )
+    }
+}
+
+
+export default PricingPage;
+// ReactDOM.render(<PricingPage />, appRoot);
+//test
---
+++ src/components/Veiws/clientBidCard.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+
+//takes props: 1)itemUPC 2)itemName 3)itemDescription 4)itemImageURL
+
+export default class Client_BIDCARD extends Component {
+    render(){
+        return(
+            <React.Fragment>
+                {/* BID CARD: will append to the page */}
+                <div id="bidCard-Container">
+                    <img id="bidCard-section-left" class="bidCard-section" src={this.props.itemImageURL}></img>
+                    <div id="bidCard-section-middle" class="bidCard-section">
+                        <label><strong>Item ID:</strong> <text>{this.props.itemUPC}</text></label>
+                        <br></br>
+                        <label><strong>Item Name: </strong><text>{this.props.itemName}</text></label>
+                        <br></br>
+                        <label><strong>Description: </strong><text>{this.props.itemDescription}</text></label>
+                    </div>
+                    <div id="bidCard-section-right" class="bidCard-section">
+                        <button class="bidResponseButton">Accept</button>
+                        <button class="bidResponseButton">Decline</button>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+//test
