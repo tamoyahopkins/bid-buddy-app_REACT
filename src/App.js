6ad7e76c4e6f7bfe67c02294dd841dde23d99e34
Shaquon <shaquonkelley@gmail.com>
2019-07-12 15:30:15 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
   }
 }
 
-// // export default App;
+export default App;
 
 // // //test
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as App } from "../App";
 export { default as Home } from "../views/Home";
 export { default as Scanner } from "./Scanner";
 
