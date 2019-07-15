6ac687724732ae9064b0031e785f49748ab2118c
Tamoya Hopkins <tamoyashopkins@gmail.com>
2019-07-15 01:59:30 -0400

    added styling to the client account page dashboard

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react"
+import {Component} from "react"
 // const appRoot = document.getElementById('root');
 const ReactDOM = require('react-dom');
 const modalRoot = document.getElementById('modal-root');
@@ -19,49 +19,5 @@ class Modal extends Component {
   }
 }
 
-
-
-class Modal1 extends Component {
-  state = {showModal: false};
-  
-  handleShow = ()=> {
-    this.setState({showModal: true });
-  }
-  handleClose = ()=> {
-    let newShowModal = !this.state.showModal
-    this.setState({showModal: newShowModal }); 
-  }
-  render() {
-    return (
-      <Modal>
-          <div className="modal">
-            <img class="modal_clientLogo" src="https://s3-media4.fl.yelpcdn.com/bphoto/KImy2lcnme23Q8jeUQ7s_A/ls.jpg" ></img>
-            <div class="modal_userForm">
-              <form onSubmit={this.handleClose} id="modal_Form">
-                <label>Listed Price: </label>
-                <input></input>
-                <label>Enter Bid Amount:</label>
-                <input></input>
-                
-                <h3>Payment Details</h3>
-                <label>Payee Name:</label>
-                <input></input>
-                <label>Card Number:</label>
-                <input></input>
-                <label>Expiration Date:</label>
-                <input></input>
-                <label>CBD:</label>
-                <input></input>
-                <hr></hr>
-                <button type="submit">Confirm Bid!</button>
-              </form>
-            </div>
-          </div>
-        </Modal>
-    );
-  }
-}
-
 // ReactDOM.render(<App />, appRoot);
-export default Modal1; 
-//test
+export default Modal; 
--- src/views/ClientAccountPage.js
+++ src/views/ClientAccountPage.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react'
+// import Modal from "../components/Modals"
 import {
+    Modal, 
+    Header,
+    Button, 
+    Icon,   
   Container,
   Divider,
   Dropdown,
   Grid,
-  Header,
+  Label,
+  Input, 
   Image,
   List,
   Menu,
@@ -12,6 +18,69 @@ import {
   Form,
 } from 'semantic-ui-react'
 import logo from "../images/bid_buddy_logo_3.png";
+
+let categories = [
+    { key: 'Accessories', text: 'Accessories', value: 'Accessories' },
+    { key: 'Electronics', text: 'Electronics', value: 'Electronics' },
+    { key: 'Furniture', text: 'Furniture', value: 'Furniture' },
+    { key: 'Hardware', text: 'Hardware', value: 'Hardware' },
+    { key: 'Health & Beauty', text: 'Health & Beauty', value: 'Health & Beauty' },
+    { key: 'Home & Garden', text: 'Home & Garden', value: 'Home & Garden' },
+    { key: 'Luggage & Bags', text: 'Luggage & Bags', value: 'Luggage & Bags' },
+    { key: 'Women Shoes', text: 'Women Shoes', value: 'Womens Shoes' },
+    { key: 'Mens Shoes', text: 'Mens Shoes', value: 'Mens Shoes' },
+    { key: 'Kids Shoes', text: 'Kids Shoes', value: 'Kids Shoes' },
+    { key: 'Womens Clothing', text: 'Womens Clothing', value: 'Womens Clothing' },
+    { key: 'Mens Clothing', text: 'Mens Clothing', value: 'Mens Clothing' },
+    { key: 'Toys & Games', text: 'Toys & Games', value: 'Toys & Games'},
+]
+
+let handleSubmit = () => {
+
+}
+
+
+const addButton = () => 
+      (<Modal trigger={<Button color='dark blue'>+</Button>} closeIcon>
+    <Grid rows={2}>
+        <Grid.Row><Header as='h2' icon='hand point up' content='Accept bids automatically by setting custom thresholds below.' /></Grid.Row>
+        <Grid.Row><Header as='h4'>Set threshold by category</Header></Grid.Row>
+        <Grid.Row columns={2}>
+            <Grid.Column><Dropdown placeholder='Categories' fluid multiple selection options={categories} /></Grid.Column>
+            <Grid.Column><Input labelPosition="right" type="text" placeholder='Threshold (%)'>
+            <input/><Label basic>%</Label></Input></Grid.Column>
+        </Grid.Row>
+        <Divider/>
+        <Grid.Row><Header as='h4'>Set threshold by brand name</Header></Grid.Row>
+        <Grid.Row columns={2}>
+            <Grid.Column><Input placeholder='Enter Brand Name'/></Grid.Column>
+            <Grid.Column><Input labelPosition="right" type="text" placeholder='Threshold (%)'>
+            <input/><Label basic>%</Label></Input></Grid.Column>
+        </Grid.Row>
+        <Divider/>
+       <Grid.Row> <Header as='h4'>Set threshold by volumne in stock</Header> </Grid.Row>
+        <Grid.Row columns={2}>
+            <Grid.Column><Input placeholder='Enter Volume Amount'/></Grid.Column>
+            <Grid.Column><Input labelPosition="right" type="text" placeholder='Threshold (%)'>
+            <input/><Label basic>%</Label></Input></Grid.Column>
+        </Grid.Row>
+        <Divider/>
+        <Grid.Row><Header as='h4'>Set threshold by age of product in stock</Header></Grid.Row>
+        <Grid.Row columns={2}>
+            <Grid.Column><Input placeholder='Age limit'/></Grid.Column>
+            <Grid.Column><Input labelPosition="right" type="text" placeholder='Threshold (%)'>
+            <input/><Label basic>%</Label></Input></Grid.Column>
+        </Grid.Row>
+        <Grid.Row><Header as='h4'>Set threshold by item number (UPC)</Header></Grid.Row>
+        <Grid.Row columns={2}>
+            <Grid.Column><Input placeholder='Item Number'/></Grid.Column>
+            <Grid.Column><Input labelPosition="right" type="text" placeholder='Threshold (%)'>
+            <input/><Label basic>%</Label></Input></Grid.Column>
+        </Grid.Row>
+        <Grid.Row><Button color='orange' primary>Set Thresholds</Button></Grid.Row>
+    </Grid>
+  </Modal>
+)
 
 const GridExampleCelledInternally = () => (
 <React.Fragment>
@@ -33,19 +102,33 @@ const GridExampleCelledInternally = () => (
         {/* feed container div */}
         <div id="feedContainer">
             <div id="leftCol">
-                <div>
-                    <h2>Product Dash</h2>       
-                    <h3 class="header-text">Catagory</h3>
-                    <button class="dash-button">+</button>
+                <div className="productDash">
+                    Product Dash
+                    <br></br>
+                    <br></br>
+                    {addButton()}      
                 </div>
-                <div>
-                    <h3 class="header-text">Brand</h3>
-                    <button class="dash-button">+</button>
+                <div className="header-text">
+                    Catagory
+                    {/* <button class="dash-button">+</button> */}
                 </div>
-                <div>
-                    <h3 class="header-text">Volume</h3>
-                    <button class="dash-button">+</button>
+                <div className="header-text">
+                    Brand
+                    {/* <button class="dash-button">+</button> */}
                 </div>
+                <div className="header-text">
+                    Volume
+                    {/* <button class="dash-button">+</button> */}
+                </div>
+                <div className="header-text">
+                    Age
+                    {/* <button class="dash-button">+</button> */}
+                </div>
+                <div className="header-text">
+                    Item Number (UPC)
+                    {/* <button class="dash-button">+</button> */}
+                </div>
+
             </div>
             <div id="rightCol">
                 {/* BID CARD: will append to the page */}
